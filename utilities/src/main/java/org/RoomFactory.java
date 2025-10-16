03d0e783a830a52e9483398d08f84aeb8eda5ee0
spencerduberry <spencerduberry@hotmail.co.uk>
2025-10-16 16:32:55 +0100

    feat: implement factory for room creation

---
+++ utilities/src/main/java/org/RoomFactory.java
@@ -0,0 +1,25 @@
+package org;
+
+public class RoomFactory {
+    private final CustomInput inputSource;
+    private final RoomTypeRepository roomRepo;
+
+    public RoomFactory(CustomInput inputSource, RoomTypeRepository roomRepo) {
+        this.inputSource = inputSource;
+        this.roomRepo = roomRepo;
+    }
+
+    public Room addRoomType() {
+        String type = inputSource.inputString();
+        String description = inputSource.inputString();
+        int minRoomNumber = inputSource.inputInt();
+        int maxRoomNumber = inputSource.inputInt();
+        String bedType = inputSource.inputString();
+        int roomTypeTotal = inputSource.inputInt();
+        int rate = inputSource.inputInt();
+
+        Room newRoom = new Room(type, description, minRoomNumber, maxRoomNumber, bedType, roomTypeTotal, rate);
+        roomRepo.addRoom(newRoom);
+        return newRoom;
+    }
+}
